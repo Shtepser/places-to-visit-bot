af3907cd266769797adcbc72266074baa090172b
Anatoliy Poletaev <shtepser@yandex.ru>
2020-05-13 15:01:01 +0300

    Fixed python version on Pipfile

--- places_memorizer.py
+++ places_memorizer.py
@@ -5,10 +5,9 @@ import telebot
 
 TOKEN = os.environ.get("API_KEY")
 memorizer = telebot.TeleBot(TOKEN)
-print(TOKEN)
 memorizer.polling(none_stop=True)
 
 
 @memorizer.message_handler()
 def echo(message):
-    memorizer.reply_to(message, message.text)
+    memorizer.reply_to(message.chat.id, message.text)
