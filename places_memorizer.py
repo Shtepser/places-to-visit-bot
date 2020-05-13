338f6b3cc2c31f6e90410f5671a11546dc530d71
Anatoliy Poletaev <shtepser@yandex.ru>
2020-05-13 15:05:17 +0300

    Fixed echo handler

--- places_memorizer.py
+++ places_memorizer.py
@@ -2,12 +2,14 @@ import os
 
 import telebot
 
-
 TOKEN = os.environ.get("API_KEY")
 memorizer = telebot.TeleBot(TOKEN)
-memorizer.polling(none_stop=True)
 
 
 @memorizer.message_handler()
 def echo(message):
+    print("Reply to %s" % message.chat.id)
     memorizer.reply_to(message.chat.id, message.text)
+
+
+memorizer.polling(none_stop=True)
