0ab9f33d044f4951e9c74333fdac023f76ac04fd
Anatoliy Poletaev <shtepser@yandex.ru>
2020-05-13 21:10:28 +0300

    Restored echo function for testing

--- places_memorizer.py
+++ places_memorizer.py
@@ -8,8 +8,8 @@ memorizer = telebot.TeleBot(TOKEN)
 
 @memorizer.message_handler(commands=["/start", "/help"])
 def info(message):
-    print(f"Replying to {message.chat.id}")
-    memorizer.reply_to(message, """
+    print(f"Sending help to {message.chat.id}")
+    memorizer.send_message(message.chat.id, """
     Вас приветствует Запоминатель Мест версии 0.1!
     Используйте его для запоминания информации о местах, которые хотели бы посетить в будущем.
     Команды:
@@ -19,6 +19,12 @@ def info(message):
     /visited - отметить место как посещённое
     /reset - удалить все свои запомненные места
     /help - показать эту справку""")
+
+
+@memorizer.message_handler(func=lambda x: True)
+def echo(message):
+    print(f"Echoing to {message.chat.id}")
+    memorizer.send_message(message.chat.id, message.text)
 
 
 memorizer.polling(none_stop=True)
