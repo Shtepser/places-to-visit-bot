8eb5214a9d6feeb01266a26e19862751b49e9812
Anatoliy Poletaev <shtepser@yandex.ru>
2020-05-13 20:09:13 +0300

    Added help

--- places_memorizer.py
+++ places_memorizer.py
@@ -6,10 +6,18 @@ TOKEN = os.environ.get("API_KEY")
 memorizer = telebot.TeleBot(TOKEN)
 
 
-@memorizer.message_handler()
-def echo(message):
-    print("Reply to %s" % message.chat.id)
-    memorizer.reply_to(message, message.text)
+@memorizer.message_handler(commands=["/start", "/help"])
+def info(message):
+    memorizer.reply_to(message, """
+    Вас приветствует Запоминатель Мест версии 0.1!
+    Используйте его для запоминания информации о местах, которые хотели бы посетить в будущем.
+    Команды:
+    /add - запомнить новое место
+    /list - вспомнить места, в которые можно сходить
+    /map - получить карту места для посещения
+    /visited - отметить место как посещённое
+    /reset - удалить все свои запомненные места
+    /help - показать эту справку""")
 
 
 memorizer.polling(none_stop=True)
