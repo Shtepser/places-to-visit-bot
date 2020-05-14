8fc4904793dba7f65b5aad3f67d96bbd17cfeb87
Anatoliy Poletaev <shtepser@yandex.ru>
2020-05-14 17:34:39 +0300

    Sets in database replaced with hashes

--- database.py
+++ database.py
@@ -6,25 +6,27 @@ from Place import Place
 from Stage import Stage
 
 
+REDIS_URL = os.environ.get("REDIS_URL")
+
+
 class Database:
 
     def __init__(self):
-        redis_URL = os.environ.get("REDIS_URL")
-        self.__places = redis.from_url(redis_URL, db=0)
-        self.__stages = redis.from_url(redis_URL, db=1)
+        self.__places = redis.from_url(REDIS_URL, db=0)
+        self.__stages = redis.from_url(REDIS_URL, db=1)
 
     def has_user(self, user_id) -> bool:
-        return bool(self.__places.smembers(user_id)) and self.__stages.get(user_id) is not None
+        return bool(self.__places.hkeys(user_id)) and self.__stages.get(user_id) is not None
 
     def add_place(self, user_id, place: Place):
-        self.__places.sadd(user_id, repr(place))
+        self.__places.hset(user_id, place.name, repr(place))
 
     def get_places(self, user_id) -> list:
-        return [Place.from_string(place.decode("utf8"))
-                for place in self.__places.smembers(user_id)]
+        return [Place.from_string(self.__places.hget(user_id, key).decode("utf8"))
+                for key in self.__places.hkeys(user_id)]
 
     def remove_place(self, user_id, place: Place):
-        return self.__places.srem(user_id, repr(place)) == 1
+        self.__places.hdel(user_id, place.name)
 
     def reset_user(self, user_id):
         self.__places.delete(user_id)
